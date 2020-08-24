1ecf75db75c57ea33dfbbf52265a34c2bc5e0b17
Valerie Osei <vosei@FreedomMobile.ca>
2020-08-24 10:43:47 -0400

    completed basic form structure

---
+++ world-bank-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "./Form";
+import NavBar from "./NavBar";
+import Output from "./Output";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Form />
+      <Output />
+    </div>
+  );
+}
+
+export default App;
